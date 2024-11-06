c8706b2ce097593b7c7c3d0c537d5dd675aa24c3
Daniel <dan@gmail.com>
2024-11-06 18:22:30 +0000

    mysql connector

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,19 @@
+#!/ust/bin/env python3
+""""""
+import os
+import mysql.connector
+
+
+def get_db():
+    """Returns a connector to MySQL database server."""
+
+    user = os.environ.get('PERSONAL_DATA_DB_USERNAME')
+    passwd = os.environ.get('PERSONAL_DATA_DB_PASSWORD')
+    host = os.environ.get('PERSONAL_DATA_DB_HOST')
+    db_name = os.environ.get('PERSONAL_DATA_DB_NAME')
+
+    # Create a database connection to MySQL server connect()
+    db = mysql.connector.connect(
+            database=db_name, host=host, user=user, passwd=passwd)
+
+    return db
