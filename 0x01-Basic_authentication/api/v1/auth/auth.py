9cc26793b396e44c6b53edb13e0913c18794bcdf
dan@gmail.com <dan@gmail.com>
2024-11-12 18:50:57 +0000

    Auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,9 +8,23 @@ from typing import List, TypeVar
 class Auth:
     """Template for all authentication system"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Returns true if path is in exluded_paths otherwise false"""
-        return False
-    
+        """Returns true if path is in excluded_paths otherwise false
+           The method is slash tolerant meaning path=/api/v1/status and 
+           path=/api/v1/status/ must be treated as same.
+        """
+        if path is None or (excluded_paths is None or excluded_paths == []):
+            return True
+
+        if not path.endswith('/'):
+            path = path + '/'
+
+        for each in excluded_paths:
+            if not each.endswith('/'):
+                each = each + '/'
+            if path == each:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """"""
