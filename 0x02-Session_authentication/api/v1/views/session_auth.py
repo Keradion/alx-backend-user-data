136357b19ba24352f968aeb90d115f6e57bab793
Daniel <danielshitaye10@gmail.com>
2024-11-17 17:42:36 +0300

    user

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -23,22 +23,26 @@ def login():
         return ({"error": "password missing"}), 400
 
     # Retrieve User instance based on the email provided
-    users = User.search({'email': email})
-    if users or len(users) > 0:
-        # Validate the provided password
-        is_pwd_valid = users[0].is_valid_password(password)
-        if not is_pwd_valid:
-            return ({"error": "wrong password"}), 401
-    else:
-        return ({"error": "no user found for this email"}), 404
+    try:
+        find_user = User.search({'email': email})
+    except Exception:
+        return jsonify({"error": "no user found for this email"}), 404
+    
+    if not users:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    for user in find_user:
+        if not user.is_valid_password(password):
+            return jsonify({"error": "wrong password"}), 401
 
     # If the user founds and valid password provide, create a session Id
     from api.v1.app import auth
-    user_id = users[0].id
+    user = find_users[0]
+    user_id = user.id
     session_id = auth.create_session(user_id)
     # Jsonfiy to create a response object to set a cookie value
     cookie_name = os.getenv('SESSION_NAME')
-    response_object = jsonify(users[0].to_json())
+    response_object = jsonify(user.to_json())
     response_object.set_cookie(cookie_name, session_id)
 
     return response_object
