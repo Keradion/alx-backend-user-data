24399b98acd1ce03c2ed212c4c2e49c88b2259b9
Daniel <danielshitaye10@gmail.com>
2024-11-20 15:34:53 +0300

    update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -60,3 +60,20 @@ class DB:
             raise NoResultFound
 
         return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+           Call find_user_id to locate the user to update.
+           update the user's attributes as passed in the arguments.
+           if an argument not valid for the object raise ValueError.
+        """
+        valid_attrs = [
+                'id', 'email', 'hashed_password', 'reset_token', 'session_id']
+        # Get user to update
+        search_by = {'id': user_id}
+        user = self.find_user_by(**search_by)
+        # check if all arguments are valid for user object
+        for key, value in kwargs.items():
+            if key not in valid_attrs:
+                raise ValueError
+            setattr(user, key, value)
